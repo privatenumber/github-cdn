fe2aecd92ca5dce4e5697ef6333d52cbed893751
Hiroki Osame <osame@squareup.com>
2020-01-13 16:57:29 -0500

    Added Debug

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 import 'dotenv/config';
 import express from 'express';
 import assert from 'assert';
+import Debug from 'debug';
 import routes from './routes';
 
+const debug = Debug('github-cdn');
 const { PORT } = process.env;
 assert(PORT, 'process.env.PORT not defined');
 
 const app = express();
 app.disable('x-powered-by');
 app.use(routes);
-app.listen(PORT, () => console.log(`Git CDN listening on ${PORT}!`));
+app.listen(PORT, () => debug(`Github CDN listening on ${PORT}!`));
--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,13 @@
-import express from 'express';
+import { Router } from 'express';
 import getRepo from './getRepo';
 import resolveSemver from './resolveSemver';
 import serveBadgen from './serveBadgen';
 import getPath from './getPath';
 
-const router = express.Router();
+const router = Router();
 
 router.get('/:owner/:repo', getRepo);
+
 router.get(
 	'/:owner/:repo/:ref:path(/*)?',
 	resolveSemver,
