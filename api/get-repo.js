9c8975a4bd2d1165c2a445211d3a5534fbbc03a3
Hiroki Osame <osame@squareup.com>
2020-05-03 03:46:59 -0400

    Fixed express router

--- api/get-repo.js
+++ api/get-repo.js
@@ -1,15 +1,12 @@
 const getRemoteInfo = require('../lib/github.get-remote-info');
 
-module.exports = async (req, res) => {
-	try {
-		const { source, data } = await getRemoteInfo(req.query);
-		res.setHeader('X-GITHUB-CDN-SOURCE', source);
-		res.send(data);
-	} catch (err) {
-		res
-			.status(422)
-			.send({
-				err: err.message,
-			});
-	}
+module.exports = (req, res) => {
+	getRemoteInfo({ ...req.query, ...req.params }).then(
+		({ source, data }) => {
+			res.setHeader('X-GITHUB-CDN-SOURCE', source);
+			res.setHeader('Cache-Control', 'public, max-age=60, immutable');
+			res.send(data);
+		},
+		(err) => res.status(422).send({ err: err.message }),
+	);
 };
