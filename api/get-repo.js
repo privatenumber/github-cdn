9132dbf7aeebaffc32c98d57db93a00f25675dee
Hiroki Osame <hiroki.osame@gmail.com>
2020-05-03 01:27:43 -0400

    Pull MD from github

--- api/get-repo.js
+++ api/get-repo.js
@@ -1,7 +1,6 @@
 const getRemoteInfo = require('../lib/github.get-remote-info');
 
 module.exports = async (req, res) => {
-	console.log('get-repo');
 	try {
 		const { source, data } = await getRemoteInfo(req.query);
 		res.setHeader('X-GITHUB-CDN-SOURCE', source);
