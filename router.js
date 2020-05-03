2c70d660d87c8e31f5468ff53eb603b62c681d3b
Hiroki Osame <hiroki.osame@gmail.com>
2020-05-03 04:10:20 -0400

    Reuse serve-landing

--- router.js
+++ router.js
@@ -1,33 +1,18 @@
-import { Router } from 'express';
-import landingTpl from './lib/landing-tpl';
-import getRepo from './api/get-repo';
-import getPath from './api/get-path';
+const { Router } = require('express');
+const serveLanding = require('./api/serve-landing');
+const getRepo = require('./api/get-repo');
+const getPath = require('./api/get-path');
 
 const router = Router();
 
-router.get('/', (req, res) => {
-	res.header('Cache-Control', 'public, max-age=31536000, immutable');
-	res.send(landingTpl);
-});
+router.get('/', serveLanding);
 
 router.use((req, res, next) => {
 	res.header('Access-Control-Allow-Origin', '*');
 	next();
 });
 
-// import { canAccess } from '../lib/utils/config';
-// router.get('/:owner/:repo*', (req, res, next) => {
-// 	if (canAccess(req.params)) {
-// 		next();
-// 		return;
-// 	}
-
-// 	res
-// 		.status(401)
-// 		.send({ error: 'Unauthorized access' });
-// });
-
 router.get('/:owner/:repo/:ref?:path(/*)?', getPath);
 router.get('/:owner/:repo?', getRepo);
 
-export default router;
+module.exports = router;
