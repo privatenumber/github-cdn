91eae7c9fdc34531a57a97d1fdfee1667086b3ec
Hiroki Osame <hiroki.osame@gmail.com>
2020-05-03 19:25:22 -0400

    Update readme

--- api/serve-landing.js
+++ api/serve-landing.js
@@ -29,7 +29,7 @@ const landingTpl = `
 	<div id="md" class="markdown-body">Loading...</div>
 	<script src="https://cdn.jsdelivr.net/npm/js-cookie@rc/dist/js.cookie.min.js"></script>
 	<script type="text/javascript">
-	fetch('https://github-cdn.now.sh/privatenumber/github-cdn/master/readme.md')
+	fetch('https://github-cdn.now.sh/privatenumber/github-cdn/master/readme.md?token')
 		.then(r => r.text())
 		.then(mdStr => {
 			md.innerHTML = marked(mdStr);
