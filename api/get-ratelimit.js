e3e052186515d1a9234af9d1d830fc46f703a17d
Hiroki Osame <hiroki.osame@gmail.com>
2020-05-03 20:01:24 -0400

    Error handling for rate limit

--- api/get-ratelimit.js
+++ api/get-ratelimit.js
@@ -5,12 +5,22 @@ const { github } = require('../lib/utils/config');
 module.exports = async (req, res) => {
 	log('[req:get-ratelimit]', req.url);
 
-	const { token = github.token } = req.cookies;
-	const opts = token ? { headers: { Authorization: `token ${token}` } } : undefined;
-	const data = await githubApi('rate_limit').json();
+	const token = req.cookies.token || github.token;
 
-	res.send({
-		token_source: (req.cookies.token ? 'Cookie' : ( github.token ? 'Server' : 'N/A')),
-		rate: data.resources.core,
-	});
+	const opts = token ? { headers: { Authorization: `token ${token}` } } : undefined;
+
+	githubApi('rate_limit', opts).json().then(
+		(data) => {
+			if (data.message) {
+				return res.status(500).send({ err: data.message });
+			}
+			res.send({
+				token_source: (req.cookies.token ? 'Cookie' : ( github.token ? 'Server' : 'N/A')),
+				rate: data.resources.core,
+			});
+		},
+		(err) => {
+			res.status(500).send({ err: 'Error fetching rate limit data' });
+		},
+	);
 };
